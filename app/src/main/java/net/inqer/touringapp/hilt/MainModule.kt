a1a8d15120f28101addd4aa8573586a7a5da5cf1
MaxKayan <maxim.kayander1@gmail.com>
2021-03-18 16:55:30 +0300

    Added main repository provider

--- app/src/main/java/net/inqer/touringapp/hilt/MainModule.kt
+++ app/src/main/java/net/inqer/touringapp/hilt/MainModule.kt
@@ -7,7 +7,13 @@ import dagger.hilt.InstallIn
 import dagger.hilt.android.components.ViewModelComponent
 import dagger.hilt.android.qualifiers.ApplicationContext
 import dagger.hilt.android.scopes.ViewModelScoped
+import kotlinx.coroutines.CoroutineDispatcher
+import kotlinx.coroutines.Dispatchers
 import net.inqer.touringapp.R
+import net.inqer.touringapp.data.remote.RoutesApi
+import net.inqer.touringapp.data.repository.main.DefaultMainRepository
+import net.inqer.touringapp.data.repository.main.MainRepository
+import net.inqer.touringapp.util.DispatcherProvider
 import javax.inject.Named
 
 @Module
@@ -22,4 +28,23 @@ object MainModule {
             @Named("String1") testString1: String
     ) = "${context.getString(R.string.string_to_inject)} - $testString1"
 
+
+    @ViewModelScoped
+    @Provides
+    fun provideMainRepository(api: RoutesApi): MainRepository = DefaultMainRepository(api)
+
+
+    @ViewModelScoped
+    @Provides
+    fun provideDispatchers(): DispatcherProvider = object : DispatcherProvider {
+        override val main: CoroutineDispatcher
+            get() = Dispatchers.Main
+        override val io: CoroutineDispatcher
+            get() = Dispatchers.IO
+        override val default: CoroutineDispatcher
+            get() = Dispatchers.Main
+        override val unconfined: CoroutineDispatcher
+            get() = Dispatchers.Unconfined
+
+    }
 }
