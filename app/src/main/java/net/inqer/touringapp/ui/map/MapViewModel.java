f25529d75183300dbccdf7c2a318d42a1c091121
MaxKayan <maxim.kayander1@gmail.com>
2021-03-18 14:01:11 +0300

    Renamed fragments

--- app/src/main/java/net/inqer/touringapp/ui/map/MapViewModel.java
+++ app/src/main/java/net/inqer/touringapp/ui/map/MapViewModel.java
@@ -1,16 +1,16 @@
-package net.inqer.touringapp.ui.notifications;
+package net.inqer.touringapp.ui.map;
 
 import androidx.lifecycle.LiveData;
 import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModel;
 
-public class NotificationsViewModel extends ViewModel {
+public class MapViewModel extends ViewModel {
 
     private MutableLiveData<String> mText;
 
-    public NotificationsViewModel() {
+    public MapViewModel() {
         mText = new MutableLiveData<>();
-        mText.setValue("This is notifications fragment");
+        mText.setValue("This is dashboard fragment");
     }
 
     public LiveData<String> getText() {
