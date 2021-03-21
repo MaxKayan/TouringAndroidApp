14aa7df40ba47b6b8451674a5622989a919f22c4
MaxKayan <maxim.kayander1@gmail.com>
2021-03-22 00:00:46 +0300

    Added a shared preferences provider

--- app/src/main/java/net/inqer/touringapp/hilt/AppModule.kt
+++ app/src/main/java/net/inqer/touringapp/hilt/AppModule.kt
@@ -1,10 +1,14 @@
 package net.inqer.touringapp.hilt
 
+import android.content.Context
+import android.content.SharedPreferences
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
+import dagger.hilt.android.qualifiers.ApplicationContext
 import dagger.hilt.components.SingletonComponent
 import net.inqer.touringapp.AppConfig
+import net.inqer.touringapp.R
 import net.inqer.touringapp.data.remote.RoutesApi
 import retrofit2.Converter
 import retrofit2.Retrofit
@@ -39,4 +43,11 @@ object AppModule {
     @Singleton
     @Provides
     fun provideRoutesApi(retrofit: Retrofit): RoutesApi = retrofit.create(RoutesApi::class.java)
+
+    @Singleton
+    @Provides
+    fun provideSharedPreferences(@ApplicationContext context: Context): SharedPreferences =
+            context.getSharedPreferences(
+                    context.getString(R.string.main_preference_file_key), Context.MODE_PRIVATE
+            )
 }
