680cd9cbe7bb0dedd5f5af831ebfbf23508006d7
MaxKayan <maxim.kayander1@gmail.com>
2021-03-29 16:50:07 +0300

    Added date converter

---
+++ app/src/main/java/net/inqer/touringapp/data/converters/DateConverter.kt
@@ -0,0 +1,16 @@
+package net.inqer.touringapp.data.converters
+
+import androidx.room.TypeConverter
+import java.util.*
+
+object DateConverter {
+    @TypeConverter
+    fun toDate(dateLong: Long?): Date? {
+        return dateLong?.let { Date(it) }
+    }
+
+    @TypeConverter
+    fun fromDate(date: Date?): Long? {
+        return date?.time
+    }
+}
