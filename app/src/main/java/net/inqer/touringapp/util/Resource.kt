9b248605bcbc7ccc1951ba074e4df702c7305e72
MaxKayan <maxim.kayander1@gmail.com>
2021-03-31 00:30:53 +0300

    Added Update event specially for the refresh method

--- app/src/main/java/net/inqer/touringapp/util/Resource.kt
+++ app/src/main/java/net/inqer/touringapp/util/Resource.kt
@@ -2,6 +2,7 @@ package net.inqer.touringapp.util
 
 sealed class Resource<T>(val data: T?, val message: String?) {
     class Success<T>(data: T) : Resource<T>(data, null)
+    class Updated<T> : Resource<T>(null, null)
     class Error<T>(message: String) : Resource<T>(null, message)
     class Loading<T>(message: String? = null) : Resource<T>(null, message)
     class Empty<T> : Resource<T>(null, null)
