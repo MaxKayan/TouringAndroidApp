96979efab1353f51f0df4dfb496bf9c00eeea0de
MaxKayan <maxim.kayander1@gmail.com>
2021-03-30 16:36:26 +0300

    Added list-to-json converters

---
+++ app/src/main/java/net/inqer/touringapp/data/converters/TourItemConverters.kt
@@ -0,0 +1,20 @@
+package net.inqer.touringapp.data.converters
+
+import androidx.room.TypeConverter
+import com.google.gson.Gson
+import net.inqer.touringapp.data.models.Destination
+import net.inqer.touringapp.data.models.Waypoint
+
+class TourItemConverters {
+    @TypeConverter
+    fun waypointsToJson(value: Array<Waypoint>): String = Gson().toJson(value, Array<Waypoint>::class.java)
+
+    @TypeConverter
+    fun jsonToWaypoints(json: String): Array<Waypoint> = Gson().fromJson(json, Array<Waypoint>::class.java)
+
+    @TypeConverter
+    fun destinationsToJson(value: Array<Destination>): String = Gson().toJson(value, Array<Destination>::class.java)
+
+    @TypeConverter
+    fun jsonToDestinations(json: String): Array<Destination> = Gson().fromJson(json, Array<Destination>::class.java)
+}
