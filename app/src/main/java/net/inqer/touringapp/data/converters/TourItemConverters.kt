cd64907735d44eb8593500c5840500dfaf8767a7
MaxKayan <maxim.kayander1@gmail.com>
2021-03-31 16:47:53 +0300

    Type converters should accept nullable values

--- app/src/main/java/net/inqer/touringapp/data/converters/TourItemConverters.kt
+++ app/src/main/java/net/inqer/touringapp/data/converters/TourItemConverters.kt
@@ -7,14 +7,14 @@ import net.inqer.touringapp.data.models.Waypoint
 
 class TourItemConverters {
     @TypeConverter
-    fun waypointsToJson(value: Array<Waypoint>): String = Gson().toJson(value, Array<Waypoint>::class.java)
+    fun waypointsToJson(value: Array<Waypoint>?): String? = Gson().toJson(value, Array<Waypoint>::class.java)
 
     @TypeConverter
-    fun jsonToWaypoints(json: String): Array<Waypoint> = Gson().fromJson(json, Array<Waypoint>::class.java)
+    fun jsonToWaypoints(json: String?): Array<Waypoint>? = Gson().fromJson(json, Array<Waypoint>::class.java)
 
     @TypeConverter
-    fun destinationsToJson(value: Array<Destination>): String = Gson().toJson(value, Array<Destination>::class.java)
+    fun destinationsToJson(value: Array<Destination>?): String? = Gson().toJson(value, Array<Destination>::class.java)
 
     @TypeConverter
-    fun jsonToDestinations(json: String): Array<Destination> = Gson().fromJson(json, Array<Destination>::class.java)
+    fun jsonToDestinations(json: String?): Array<Destination>? = Gson().fromJson(json, Array<Destination>::class.java)
 }
