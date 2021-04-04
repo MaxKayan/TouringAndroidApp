23d85c5b09e6ee2cdfcda8926e394808196f8f17
MaxKayan <maxim.kayander1@gmail.com>
2021-04-04 23:29:21 +0300

    Added new dao methods

--- app/src/main/java/net/inqer/touringapp/data/local/dao/TourRouteDao.kt
+++ app/src/main/java/net/inqer/touringapp/data/local/dao/TourRouteDao.kt
@@ -1,15 +1,28 @@
 package net.inqer.touringapp.data.local.dao
 
 import androidx.room.Dao
+import androidx.room.Insert
 import androidx.room.Query
+import androidx.room.Update
 import kotlinx.coroutines.flow.Flow
 import net.inqer.touringapp.data.models.TourRoute
+import net.inqer.touringapp.data.models.TourRouteBrief
 
 @Dao
 interface TourRouteDao : BaseDao<TourRoute> {
+
+    @Update(entity = TourRoute::class)
+    suspend fun updateByBriefList(list: List<TourRouteBrief>?)
+
+    @Insert(entity = TourRoute::class)
+    suspend fun createByBriefList(list: List<TourRouteBrief>?)
+
     @Query("SELECT * FROM routes WHERE id = :id")
     suspend fun getRoute(id: Long): TourRoute
 
     @Query("SELECT * FROM routes")
     fun getRoutesFlow(): Flow<List<TourRoute>>
+
+    @Query("SELECT * FROM routes")
+    suspend fun getRoutesList(): List<TourRoute>
 }
