56c08aee2dc255d3a4f7a76c307e96d078fcac50
MaxKayan <maxim.kayander1@gmail.com>
2021-04-04 23:28:15 +0300

    Improved the generic api call method

--- app/src/main/java/net/inqer/touringapp/data/repository/Repository.kt
+++ app/src/main/java/net/inqer/touringapp/data/repository/Repository.kt
@@ -1,16 +1,40 @@
 package net.inqer.touringapp.data.repository
 
+import android.util.Log
+import kotlinx.coroutines.flow.MutableStateFlow
 import net.inqer.touringapp.util.Resource
 import retrofit2.Response
 
 abstract class Repository {
-    protected fun <T> processResponse(response: Response<T>): Resource<T> {
-        val result = response.body()
+    protected suspend fun <T> processResponse(
+            apiCall: suspend () -> Response<T>,
+            events: MutableStateFlow<Resource<List<T>>>? = null,
+            onSuccess: (suspend (result: T) -> Unit)? = null,
+            onError: ((e: Exception) -> Unit)? = null
+    ): Resource<T> {
+        try {
+            val response = apiCall()
+            val body = response.body()
 
-        return if (response.isSuccessful && result != null) {
-            Resource.Success(result)
-        } else {
-            Resource.Error(response.message())
+            return if (response.isSuccessful && body != null) {
+                events?.value = Resource.Updated()
+                onSuccess?.invoke(body)
+                Resource.Success(body)
+            } else {
+                Log.e(TAG, "refreshTourRoutes: the response was not successful" +
+                        " ${response.message()}")
+                events?.value = Resource.Error(response.message())
+                Resource.Error(response.message())
+            }
+        } catch (e: Exception) {
+            Log.e(TAG, "refreshTourRoutes: failed to fetch", e)
+            events?.value = Resource.Error(e.message ?: "Ошибка загрузки данных!")
+            onError?.invoke(e)
+            return Resource.Error(e.message ?: "Ошибка загрузки данных!")
         }
+    }
+
+    companion object {
+        private const val TAG = "Repository"
     }
 }
