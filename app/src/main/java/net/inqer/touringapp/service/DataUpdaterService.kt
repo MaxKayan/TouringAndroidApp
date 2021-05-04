ab475c9ccc6daeb0529bcd1502cbd51165844ef7
MaxKayan <maxim.kayander1@gmail.com>
2021-05-04 22:46:26 +0300

    Added DI. Added few imports.

--- app/src/main/java/net/inqer/touringapp/service/DataUpdaterService.kt
+++ app/src/main/java/net/inqer/touringapp/service/DataUpdaterService.kt
@@ -1,13 +1,30 @@
 package net.inqer.touringapp.service
 
+import android.app.Notification
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.content.Context
 import android.content.Intent
 import android.util.Log
 import androidx.lifecycle.LifecycleService
+import androidx.lifecycle.lifecycleScope
+import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.launch
+import net.inqer.touringapp.data.repository.main.MainRepository
+import javax.inject.Inject
 
 /**
  * Updates full [net.inqer.touringapp.data.models.TourRoute] data from the API.
  */
+@AndroidEntryPoint
 class DataUpdaterService : LifecycleService() {
+
+    @Inject
+    lateinit var notificationManager: NotificationManager
+
+    @Inject
+    lateinit var routeRepository: MainRepository
+
     override fun onCreate() {
         super.onCreate()
         Log.d(TAG, "onCreate: called")
