fdc2cceac8ed8bafae5c527b2b1a5c557821fbbe
MaxKayan <maxim.kayander1@gmail.com>
2021-05-04 22:48:00 +0300

    Added necessary private methods, static values. Added static public function that starts sends the start service intent.

--- app/src/main/java/net/inqer/touringapp/service/DataUpdaterService.kt
+++ app/src/main/java/net/inqer/touringapp/service/DataUpdaterService.kt
@@ -5,7 +5,11 @@ import android.app.NotificationChannel
 import android.app.NotificationManager
 import android.content.Context
 import android.content.Intent
+import android.os.Build
 import android.util.Log
+import androidx.annotation.RequiresApi
+import androidx.core.app.NotificationCompat
+import androidx.core.content.ContextCompat
 import androidx.lifecycle.LifecycleService
 import androidx.lifecycle.lifecycleScope
 import dagger.hilt.android.AndroidEntryPoint
@@ -37,7 +41,63 @@ class DataUpdaterService : LifecycleService() {
         return super.onStartCommand(intent, flags, startId)
     }
 
+    private fun updateRouteData(routeId: Long) {
+        lifecycleScope.launchWhenCreated {
+            launch {
+                routeRepository.refreshFullRouteData(routeId)
+                Log.d(TAG, "updateRouteData: finished.")
+                stopSelf()
+            }
+        }
+    }
+
+
+    /**
+     * Create the foreground service notification channel for API >= 26
+     */
+    @RequiresApi(Build.VERSION_CODES.O)
+    private fun createNotificationChannel() {
+        val channel =
+            NotificationChannel(
+                CHANNEL_ID,
+                CHANNEL_NAME,
+                NotificationManager.IMPORTANCE_HIGH
+            )
+
+        notificationManager.createNotificationChannel(channel)
+    }
+
+
+    private fun createNotification(): Notification {
+        return NotificationCompat.Builder(this, CHANNEL_ID).let {
+            it.setContentTitle("Синхронизация данных маршрута...")
+            it.setProgress(100, 50, true)
+
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+                it.setChannelId(CHANNEL_ID)
+                it.priority = NotificationManager.IMPORTANCE_LOW
+            }
+
+            return@let it.build()
+        }
+    }
+
     companion object {
         private const val TAG = "DataUpdaterService"
+        private const val EXTRA_ROUTE_ID = "EXTRA_ROUTE_ID"
+
+        private const val CHANNEL_ID = "TOUR_DATA_UPDATER_SERVICE_ID"
+        private const val CHANNEL_NAME = "TOUR_DATA_UPDATER_SERVICE_NAME"
+
+        private const val NOTIFICATION_ID = 131214
+
+        fun initiateRouteSync(context: Context, routeId: Long) {
+            ContextCompat.startForegroundService(
+                context,
+                Intent(context, DataUpdaterService::class.java).apply {
+                    this.putExtra(EXTRA_ROUTE_ID, routeId)
+                }
+            )
+        }
     }
 }
