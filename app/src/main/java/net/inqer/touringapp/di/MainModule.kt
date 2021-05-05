6b5c62fa02524469325873387864cbfe803f6ef9
MaxKayan <maxim.kayander1@gmail.com>
2021-05-05 16:28:07 +0300

    Added ServiceComponent so MainModule.kt can be also used from services. Removed ViewModelScoped annotations for the successful build. #1

--- app/src/main/java/net/inqer/touringapp/di/MainModule.kt
+++ app/src/main/java/net/inqer/touringapp/di/MainModule.kt
@@ -3,10 +3,8 @@ package net.inqer.touringapp.di
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
+import dagger.hilt.android.components.ServiceComponent
 import dagger.hilt.android.components.ViewModelComponent
-import dagger.hilt.android.scopes.ViewModelScoped
-import kotlinx.coroutines.CoroutineDispatcher
-import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.flow.Flow
 import net.inqer.touringapp.data.local.AppDatabase
 import net.inqer.touringapp.data.local.dao.TourRouteDao
@@ -14,22 +12,19 @@ import net.inqer.touringapp.data.models.TourRoute
 import net.inqer.touringapp.data.repository.main.DefaultMainRepository
 import net.inqer.touringapp.data.repository.main.MainRepository
 import net.inqer.touringapp.di.qualifiers.ActiveTourRouteFlow
-import net.inqer.touringapp.util.DispatcherProvider
 
 @Module
-@InstallIn(ViewModelComponent::class)
+@InstallIn(ViewModelComponent::class, ServiceComponent::class)
 object MainModule {
 
-    @ViewModelScoped
     @Provides
     fun provideMainRepository(repository: DefaultMainRepository): MainRepository = repository
 
-    @ViewModelScoped
     @Provides
     fun provideTourRouteDao(database: AppDatabase): TourRouteDao = database.tourRouteDao()
 
-    @ViewModelScoped
     @Provides
     @ActiveTourRouteFlow
-    fun provideActiveRouteFlow(repository: MainRepository): Flow<TourRoute?> = repository.observeActiveRoute()
+    fun provideActiveRouteFlow(repository: MainRepository): Flow<TourRoute?> =
+        repository.observeActiveRoute()
 }
